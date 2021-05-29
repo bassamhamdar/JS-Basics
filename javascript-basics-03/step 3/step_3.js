f9341eec02f30423b64d50e844326da02638e08b
bassam hamdar <hamdar.m.bassam@gmail.com>
2021-05-29 18:58:25 +0300

    step3

---
+++ javascript-basics-03/step 3/step_3.js
@@ -0,0 +1,10 @@
+document.querySelector('.green').addEventListener('click', ()=>{
+    document.querySelector('p').style.color = "green";
+});
+
+document.querySelector('.red').addEventListener('click', ()=>{
+    document.querySelector('p').style.color = "red";
+});
+document.querySelector('.blue').addEventListener('click', ()=>{
+    document.querySelector('p').style.color = "blue";
+});
