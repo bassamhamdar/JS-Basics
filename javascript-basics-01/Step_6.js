65d6f1225a0880bb0d94a06790f86b4e0f5f46fa
bassam hamdar <hamdar.m.bassam@gmail.com>
2021-05-29 14:14:08 +0300

    step6 fixed

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,6 +1,5 @@
-function Remainder(){
-    var n1 = document.getElementById("first_number").value;
-    var n2 = document.getElementById("second_number").value;
-    var result = n1 % n2;
-    alert("Remainder: "+result);
+function remainder() {
+var n1 = document.getElementById('first_number').value;
+var n2 = document.getElementById('second_number').value;
+alert(n1 % n2);
 }
