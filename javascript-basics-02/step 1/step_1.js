20945cad07e775e130e862f1b3064dbd60fb4679
bassam hamdar <hamdar.m.bassam@gmail.com>
2021-05-29 15:00:24 +0300

    step1 fixed

--- javascript-basics-02/step 1/step_1.js
+++ javascript-basics-02/step 1/step_1.js
@@ -1,7 +1,7 @@
 function NewImage() { 
-    document.getElementById('image1').src="images/image1_2.jpg"
+    document.querySelector('img').src="images/image1_2.jpg"
 }
 
 function OldImage() {
-    document.getElementById('image1').src="images/image1.jpg"
+    document.querySelector('img').src="images/image1.jpg"
 }
