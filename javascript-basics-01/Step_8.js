7b22e71a50e9804f3ff5cd32796ce47260d84fe7
bassam hamdar <hamdar.m.bassam@gmail.com>
2021-05-29 14:31:11 +0300

    step8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function yourage() {
+    var age = document.getElementById('age').value;
+    if (age > 18){
+        alert("you are over 18");
+    }else{
+        alert("you are under 18");
+    }
+
+}
