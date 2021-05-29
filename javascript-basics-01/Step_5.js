b765ec1477109b68f8f1e86493731490e847b039
bassam hamdar <hamdar.m.bassam@gmail.com>
2021-05-29 12:11:38 +0300

    step5 corrected

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,6 @@
-var n1 = prompt("enter first number ");
-var n2 = prompt("enter second number");
-var mult = n1 * n2;
-alert(n1 + " multiplied by "+n2+" = "+mult);
+function ShowResult() {
+    var n1 = document.getElementById('first_number').value;
+    var n2 = document.getElementById('second_number').value;
+    var mult = n1 * n2;
+    alert(n1 + " multiplied by "+n2+" = "+mult);
+}
