073c2ea50065c5a5d1464075cf268d33f8d5363c
bassam hamdar <hamdar.m.bassam@gmail.com>
2021-05-29 12:49:07 +0300

    step6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function Remainder(){
+    var n1 = document.getElementById("first_number").value;
+    var n2 = document.getElementById("second_number").value;
+    var result = n1 % n2;
+    alert("Remainder: "+result);
+}
